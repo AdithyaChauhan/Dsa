2e79bdbf4ae91aab80e839168bcccc839d02c754
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2022-11-11 23:25:26 +0530

    Time: 2 ms (96.62%), Space: 41.7 MB (30.86%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        
+        //total coins is in the form of sum of first n integers
+        
+        long m = n;
+        int row = (int)(-1 + Math.sqrt(1 + 8 *m))/ 2;
+        
+        return (row);
+    }
+}
