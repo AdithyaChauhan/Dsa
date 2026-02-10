df69ef9ba2ce7cce36f16824ebfe16e4e4e7b15a
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2026-02-10 19:10:26 +0530

    Time: 0 ms (100%), Space: 20 MB (55.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,31 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        col0 = -1
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        rows, cols = len(matrix), len(matrix[0])
+
+        col0 = 1
+
+        # Step 1: mark
+        for i in range(rows):
+            if matrix[i][0] == 0:
+                col0 = 0
+            for j in range(1, cols):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
-                    if j != 0:
-                        matrix[0][j] = 0
-                    else:
-                        col0 = 0
-        for i in range(len(matrix) - 1, 0, -1):
-            for j in range(len(matrix[0]) - 1, 0, -1):
-                    if matrix[i][0] == 0 or matrix[0][j] == 0:
-                            matrix[i][j] = 0
+                    matrix[0][j] = 0
 
+        # Step 2: zero inner matrix (reverse)
+        for i in range(rows - 1, 0, -1):
+            for j in range(cols - 1, 0, -1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        # Step 3: first row
+        if matrix[0][0] == 0:
+            for j in range(cols):
+                matrix[0][j] = 0
+
+        # Step 4: first column
         if col0 == 0:
-            for i in range(len(matrix[0])):
-                matrix[0][i] = 0
-        if col0 == 0:
-            for i in range(len(matrix)):
+            for i in range(rows):
                 matrix[i][0] = 0
