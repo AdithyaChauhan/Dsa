94f9e860cb338c085579ee1e9477506753fcc20a
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2026-06-15 14:05:14 +0530

    Time: 17 ms (20.57%), Space: 19.4 MB (75.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
         if len(s) != len(t):
             return False
 
-        arr = [0] * 26
+        char_dict = {}
 
         for i in range(len(s)):
-            arr[ord(s[i]) - ord('a')] += 1
-            arr[ord(t[i]) - ord('a')] -= 1
+            char_dict[s[i]] = char_dict.get(s[i], 0) + 1
 
-        for check in arr:
-            if check != 0:
+        for j in range(len(t)):
+            if t[j] not in char_dict:
                 return False
+            else:
+                char_dict[t[j]] -= 1
+
+        for value in char_dict.values():
+            if value != 0:
+                return False
+        
         return True
