4aad3d354be7b1d92db4d0b010de5aa3c2736ce2
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2026-06-16 14:45:52 +0530

    Time: 4 ms (47.89%), Space: 19.3 MB (35.29%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+
+                if board[i][j] == '.':
+                    continue
+                if board[i][j] not in rows[i]:
+                    rows[i].add(board[i][j])
+                else:
+                    return False
+                if board[i][j] not in cols[j]:
+                    cols[j].add(board[i][j])
+                else:
+                    return False
+                if board[i][j] not in boxes[(i//3,j//3)]:
+                    boxes[(i//3,j//3)].add(board[i][j])
+                else:
+                    return False
+        
+        return True
