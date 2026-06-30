c812243c64259edb67bd94b0ca8204e65b13b308
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2026-06-30 15:16:44 +0530

    Time: 29 ms (82.52%), Space: 21.8 MB (56.11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,12 +5,9 @@ class Solution:
         prefix_sum_count = {0: 1}  # Initialize with prefix sum 0 and count 1
         
         for num in nums:
-            prefix_sum += num  # Update the running prefix sum
+            prefix_sum += num
             if (prefix_sum - k) in prefix_sum_count:
-                count += prefix_sum_count[prefix_sum - k]  # Increment count if (prefix_sum - k) is found
-            if prefix_sum in prefix_sum_count:
-                prefix_sum_count[prefix_sum] += 1  # Update the frequency of the current prefix sum
-            else:
-                prefix_sum_count[prefix_sum] = 1  # Initialize the frequency if the prefix sum is seen for the first time
-        
+                count += prefix_sum_count[prefix_sum - k]
+            prefix_sum_count[prefix_sum] = prefix_sum_count.get(prefix_sum, 0) + 1  
+            
         return count
