76bcc312fe4af57436ce92183df3d04a9c7a0e8d
Adithya Chauhan <91404342+AdithyaChauhan@users.noreply.github.com>
2026-07-11 15:58:03 +0530

    Time: 0 ms (100%), Space: 19.7 MB (24.21%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,10 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        dist = [0] * 1002
+
+        
+        dist = [0] * 1001
+        
         for numPassengers, start, end in trips:
             dist[start] += numPassengers
             dist[end] -= numPassengers
+        
         currPassengers = 0
+
         for passengers in dist:
             currPassengers += passengers
             if currPassengers > capacity:
